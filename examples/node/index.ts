2aaa071fb491cf1b5e59ebbf8581d245928f64e9
svengrim <killzoner@users.noreply.github.com>
2021-06-02 18:48:41 +0200

    Add trades streaming examples

--- examples/node/index.ts
+++ examples/node/index.ts
@@ -1,15 +1,17 @@
-import { StreamAggregatesOHLCVServiceV1Client } from '@kaiko-sdk/node/sdk/sdk_grpc_pb';
+import { StreamAggregatesOHLCVServiceV1Client, StreamTradesServiceV1Client } from '@kaiko-sdk/node/sdk/sdk_grpc_pb';
 import { InstrumentCriteria } from '@kaiko-sdk/node/sdk/core/instrument_criteria_pb';
 import { StreamAggregatesOHLCVRequestV1 } from '@kaiko-sdk/node/sdk/stream/aggregates_ohlcv_v1/request_pb';
 import { StreamAggregatesOHLCVResponseV1 } from '@kaiko-sdk/node/sdk/stream/aggregates_ohlcv_v1/response_pb';
 
 import * as grpc from '@grpc/grpc-js';
+import { StreamTradesResponseV1 } from '@kaiko-sdk/node/sdk/stream/trades_v1/response_pb';
+import { StreamTradesRequestV1 } from '@kaiko-sdk/node/sdk/stream/trades_v1/request_pb';
 
 const main = () => {
 
     // Setup authentication
     const token = process.env.KAIKO_API_KEY || '1234'; // Put your api key here
-    const metaCallback = (_params:unknown, callback:(err: Error | null, metadata?: grpc.Metadata) => void) => {
+    const metaCallback = (_params: unknown, callback: (err: Error | null, metadata?: grpc.Metadata) => void) => {
         const meta = new grpc.Metadata();
         meta.add('Authorization', `Bearer ${token}`);
         callback(null, meta);
@@ -18,9 +20,16 @@ const main = () => {
     const channelCreds = grpc.credentials.createSsl() as any;
     const callCreds = grpc.credentials.createFromMetadataGenerator(metaCallback);
     const creds = grpc.credentials.combineCallCredentials(channelCreds, callCreds);
-    const client = new StreamAggregatesOHLCVServiceV1Client('gateway-v0-grpc.kaiko.ovh:443', creds as any);
 
-    // Create a request with SDK
+    // Create a request for streaming trades with SDK
+    tradeRequest(creds);
+
+    // Create a request for streaming ohlcv with SDK
+    ohlcvRequest(creds);
+}
+
+const ohlcvRequest = (creds: grpc.CallCredentials): void => {
+    const client = new StreamAggregatesOHLCVServiceV1Client('gateway-v0-grpc.kaiko.ovh:443', creds as any);
     const request = new StreamAggregatesOHLCVRequestV1();
 
     const criteria = new InstrumentCriteria();
@@ -37,6 +46,40 @@ const main = () => {
     let count = 0;
     call.on('data', (response: StreamAggregatesOHLCVResponseV1) => {
         console.log(`aggregate: ${response.getAggregate()}, code: ${response.getCode()}, close: ${response.getClose()}`);
+        // console.log(response);
+        count++;
+        if (count >= 5) {
+            call.cancel();
+        }
+    });
+
+    call.on('end', () => {
+        console.log('Stream ended')
+    });
+
+    call.on('error', (error: grpc.ServiceError) => {
+        if (error.code === grpc.status.CANCELLED) { return; }
+        console.error(error);
+    })
+}
+
+const tradeRequest = (creds: grpc.CallCredentials): void => {
+    const client = new StreamTradesServiceV1Client('gateway-v0-grpc.kaiko.ovh:443', creds as any);
+    const request = new StreamTradesRequestV1();
+
+    const criteria = new InstrumentCriteria();
+    criteria.setExchange('cbse');
+    criteria.setInstrumentClass('spot');
+    criteria.setCode('*');
+
+    request.setInstrumentCriteria(criteria);
+
+    // Run the request and get results
+    const call = client.subscribe(request);
+
+    let count = 0;
+    call.on('data', (response: StreamTradesResponseV1) => {
+        console.log(`code: ${response.getCode()}, price: ${response.getPrice()}`);
         // console.log(response);
         count++;
         if (count >= 5) {
--- examples/python/main.py
+++ examples/python/main.py
@@ -6,14 +6,10 @@ import grpc
 
 from kaikosdk import sdk_pb2, sdk_pb2_grpc
 from kaikosdk.stream.aggregates_ohlcv_v1 import request_pb2 as pb_ohlcv
+from kaikosdk.stream.trades_v1 import request_pb2 as pb_trades
 from kaikosdk.core import instrument_criteria_pb2
 
-def run():
-    credentials = grpc.ssl_channel_credentials(root_certificates=None)
-    call_credentials = grpc.access_token_call_credentials(os.environ['KAIKO_GATEWAY_PASSWORD'])
-    composite_credentials = grpc.composite_channel_credentials(credentials, call_credentials)
-    channel = grpc.secure_channel('gateway-v0-grpc.kaiko.ovh', composite_credentials)
-
+def ohlcv_request(channel: grpc.Channel):
     try:
         with channel:
             stub = sdk_pb2_grpc.StreamAggregatesOHLCVServiceV1Stub(channel)
@@ -30,6 +26,32 @@ def run():
                 # print("Received message %s" % list(map(lambda o: o.string_value, response.data.values)))
     except grpc.RpcError as e:
         print(e.details(), e.code())
+
+def trades_request(channel: grpc.Channel):
+    try:
+        with channel:
+            stub = sdk_pb2_grpc.StreamTradesServiceV1Stub(channel)
+            responses = stub.Subscribe(pb_trades.StreamTradesRequestV1(
+                instrument_criteria = instrument_criteria_pb2.InstrumentCriteria(
+                    exchange = "cbse",
+                    instrument_class = "spot",
+                    code = "btc-usd"
+                )
+            ))
+            for response in responses:
+                print("Received message %s" % (response))
+                # print("Received message %s" % list(map(lambda o: o.string_value, response.data.values)))
+    except grpc.RpcError as e:
+        print(e.details(), e.code())
+
+def run():
+    credentials = grpc.ssl_channel_credentials(root_certificates=None)
+    call_credentials = grpc.access_token_call_credentials(os.environ['KAIKO_GATEWAY_PASSWORD'])
+    composite_credentials = grpc.composite_channel_credentials(credentials, call_credentials)
+    channel = grpc.secure_channel('gateway-v0-grpc.kaiko.ovh', composite_credentials)
+
+    trades_request(channel)
+    # ohlcv_request(channel)
 
 if __name__ == '__main__':
     logging.basicConfig()
