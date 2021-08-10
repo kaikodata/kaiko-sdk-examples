91ff151028a1fa998f3f8a85243d4f7304bfa299
Nicolas Bazire <nicolas@linux.com>
2021-08-10 23:57:29 +0200

    add more scala examples
    * vwap, spot exchange rate, market update
    * also upgrade scala dependencies to 0.0.14 from 0.0.12

--- examples/scala/src/main/scala/example/Main.scala
+++ examples/scala/src/main/scala/example/Main.scala
@@ -1,9 +1,16 @@
 package example
 
 import com.kaiko.sdk.StreamAggregatesOHLCVServiceV1Grpc
+import com.kaiko.sdk.StreamAggregatesSpotExchangeRateServiceV1Grpc
+import com.kaiko.sdk.StreamAggregatesVWAPServiceV1Grpc
+import com.kaiko.sdk.StreamMarketUpdateServiceV1Grpc
 import com.kaiko.sdk.StreamTradesServiceV1Grpc
 import com.kaiko.sdk.core.InstrumentCriteria
 import com.kaiko.sdk.stream.aggregates_ohlcv_v1.StreamAggregatesOHLCVRequestV1
+import com.kaiko.sdk.stream.aggregates_spot_exchange_rate_v1.StreamAggregatesSpotExchangeRateRequestV1
+import com.kaiko.sdk.stream.aggregates_vwap_v1.StreamAggregatesVWAPRequestV1
+import com.kaiko.sdk.stream.market_update_v1.StreamMarketUpdateCommodity
+import com.kaiko.sdk.stream.market_update_v1.StreamMarketUpdateRequestV1
 import com.kaiko.sdk.stream.trades_v1.StreamTradesRequestV1
 import io.grpc.{CallCredentials, Channel, ManagedChannelBuilder, Metadata, Status}
 
@@ -43,6 +50,36 @@ object Main {
 
     // Create a streaming ohlcv request with SDK
     ohlcv_request(channel, callCredentials)
+
+    // Create a streaming vwap request with SDK
+    vwap_request(channel, callCredentials)
+
+    // Create a streaming spot exchange rate request with SDK
+    spot_exchange_rate_request(channel, callCredentials)
+
+    // Create a streaming market update request with SDK
+    market_update_request(channel, callCredentials)
+  }
+
+  def market_update_request(channel: Channel, callCredentials: CallCredentials) = {
+    val stub = StreamMarketUpdateServiceV1Grpc.blockingStub(channel).withCallCredentials(callCredentials)
+
+    // Create a request with SDK
+    val request = StreamMarketUpdateRequestV1(
+      instrumentCriteria = Some(InstrumentCriteria(
+        exchange = "krkn",
+        instrumentClass = "spot",
+        code = "*"
+      )),
+      commodities = List(StreamMarketUpdateCommodity.SMUC_TRADE)
+    )
+
+    // Run the request and get results
+    val results = stub.subscribe(request)
+      .take(10)
+      .toSeq
+
+    println(results)
   }
 
   def ohlcv_request(channel: Channel, callCredentials: CallCredentials) = {
@@ -56,6 +93,44 @@ object Main {
         code = "btc-usd"
       )),
       aggregate = "1s",
+    )
+
+    // Run the request and get results
+    val results = stub.subscribe(request)
+      .take(10)
+      .toSeq
+
+    println(results)
+  }
+
+  def vwap_request(channel: Channel, callCredentials: CallCredentials) = {
+    val stub = StreamAggregatesVWAPServiceV1Grpc.blockingStub(channel).withCallCredentials(callCredentials)
+
+    // Create a request with SDK
+    val request = StreamAggregatesVWAPRequestV1(
+      instrumentCriteria = Some(InstrumentCriteria(
+        exchange = "cbse",
+        instrumentClass = "spot",
+        code = "btc-usd"
+      )),
+      aggregate = "1s",
+    )
+
+    // Run the request and get results
+    val results = stub.subscribe(request)
+      .take(10)
+      .toSeq
+
+    println(results)
+  }
+
+  def spot_exchange_rate_request(channel: Channel, callCredentials: CallCredentials) = {
+    val stub = StreamAggregatesSpotExchangeRateServiceV1Grpc.blockingStub(channel).withCallCredentials(callCredentials)
+
+    // Create a request with SDK
+    val request = StreamAggregatesSpotExchangeRateRequestV1(
+      aggregate = "1s",
+      code = "btc-usd"
     )
 
     // Run the request and get results
