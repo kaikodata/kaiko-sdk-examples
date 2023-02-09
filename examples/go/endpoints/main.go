8216f150a114b7a467985c5fdabf324690dd4125
killzoner <jean.grimonprez@challenger-deep.com>
2023-02-09 16:08:02 +0100

    Trades count spot live

--- examples/go/endpoints/main.go
+++ examples/go/endpoints/main.go
@@ -4,21 +4,17 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	pb "github.com/kaikodata/kaiko-go-sdk"
 	"github.com/kaikodata/kaiko-go-sdk/core"
-	"github.com/kaikodata/kaiko-go-sdk/stream/aggregated_price_v1"
-	"github.com/kaikodata/kaiko-go-sdk/stream/aggregates_direct_exchange_rate_v1"
-	"github.com/kaikodata/kaiko-go-sdk/stream/aggregates_ohlcv_v1"
-	"github.com/kaikodata/kaiko-go-sdk/stream/aggregates_spot_exchange_rate_v1"
-	"github.com/kaikodata/kaiko-go-sdk/stream/aggregates_vwap_v1"
-	"github.com/kaikodata/kaiko-go-sdk/stream/index_v1"
-	"github.com/kaikodata/kaiko-go-sdk/stream/market_update_v1"
 	"github.com/kaikodata/kaiko-go-sdk/stream/trades_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -27,7 +23,16 @@ import (
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGHUP, syscall.SIGTERM)
 	defer cancel()
+
+	// cancel context when either termination signals are received
+	go func() {
+		<-sigc
+		signal.Stop(sigc)
+		cancel()
+	}()
 
 	// Set up a connection to the server.
 	// WARNING: reconnection is automatically handled by client, you should never have
@@ -42,71 +47,17 @@ func main() {
 	apiKey := getEnv("KAIKO_API_KEY", "1234") // Put your api key here
 	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", "Bearer "+apiKey)
 
-	timeout := 15 * time.Second // demo timeout
-	ctx, cancel = context.WithTimeout(ctx, timeout)
-	defer cancel()
-
-	go func() {
-		// Create a streaming spot exchange rate request with SDK
-		err = spotExchangeRateRequest(ctx, conn)
-		if err != nil {
-			log.Fatalf("could not get spot exchange rates: %v", err)
-		}
-	}()
-
-	go func() {
-		// Create a streaming market update request with SDK
-		err := marketUpdateRequest(ctx, conn)
-		if err != nil {
-			log.Fatalf("could not get market updates: %v", err)
-		}
-	}()
-
-	go func() {
-		// Create a streaming ohlcv request with SDK
-		err := ohlcvRequest(ctx, conn)
-		if err != nil {
-			log.Fatalf("could not get ohlcvs: %v", err)
-		}
-	}()
-
-	go func() {
-		// Create a streaming vwap request with SDK
-		err := vwapRequest(ctx, conn)
-		if err != nil {
-			log.Fatalf("could not get vwaps: %v", err)
-		}
-	}()
-
-	go func() {
-		// Create a streaming direct exchange rate request with SDK
-		err := directExchangeRateRequest(ctx, conn)
-		if err != nil {
-			log.Fatalf("could not get direct exchange rates: %v", err)
-		}
-	}()
-
-	go func() {
-		// Create a streaming index request with SDK
-		err := indexRequest(ctx, conn)
-		if err != nil {
-			log.Printf("could not get index: %v", err)
-		}
-	}()
-
-	go func() {
-		// Create a streaming aggregated quote request with SDK
-		err := aggregatedQuoteRequest(ctx, conn)
-		if err != nil {
-			log.Printf("could not get aggregated quote: %v", err)
-		}
-	}()
+	start := time.Now()
 
 	// Create a streaming trades request with SDK
-	err = tradesRequest(ctx, conn)
-	if err != nil {
+	i, err := tradesRequest(ctx, conn)
+	if err != nil && errors.Is(errors.Unwrap(err), context.Canceled) {
 		log.Fatalf("could not get trades: %v", err)
 	}
+
+	end := time.Now()
+
+	fmt.Printf("%d trades between %s and %s\n", i, start.UTC().String(), end.UTC().String())
 }
 
 func getEnv(key, fallback string) string {
@@ -116,175 +67,17 @@ func getEnv(key, fallback string) string {
 	return fallback
 }
 
-func ohlcvRequest(
-	ctx context.Context,
-	conn *grpc.ClientConn,
-) error {
-	cli := pb.NewStreamAggregatesOHLCVServiceV1Client(conn)
-	request := aggregates_ohlcv_v1.StreamAggregatesOHLCVRequestV1{
-		InstrumentCriteria: &core.InstrumentCriteria{
-			Exchange:        "cbse",
-			InstrumentClass: "spot",
-			Code:            "btc-usd",
-		},
-		Aggregate: "1s",
-	}
-
-	sub, err := cli.Subscribe(ctx, &request)
-	if err != nil {
-		log.Fatalf("could not subscribe: %v", err)
-	}
-
-	for {
-		elt, err := sub.Recv()
-		if err == io.EOF {
-			return nil
-		}
-
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("[OHLCV] %+v\n", elt)
-	}
-}
-
-func vwapRequest(
-	ctx context.Context,
-	conn *grpc.ClientConn,
-) error {
-	cli := pb.NewStreamAggregatesVWAPServiceV1Client(conn)
-	request := aggregates_vwap_v1.StreamAggregatesVWAPRequestV1{
-		InstrumentCriteria: &core.InstrumentCriteria{
-			Exchange:        "cbse",
-			InstrumentClass: "spot",
-			Code:            "btc-usd",
-		},
-		Aggregate: "1s",
-	}
-
-	sub, err := cli.Subscribe(ctx, &request)
-	if err != nil {
-		log.Fatalf("could not subscribe: %v", err)
-	}
-
-	for {
-		elt, err := sub.Recv()
-		if err == io.EOF {
-			return nil
-		}
-
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("[VWAP] %+v\n", elt)
-	}
-}
-
-func directExchangeRateRequest(
-	ctx context.Context,
-	conn *grpc.ClientConn,
-) error {
-	cli := pb.NewStreamAggregatesDirectExchangeRateServiceV1Client(conn)
-	request := aggregates_direct_exchange_rate_v1.StreamAggregatesDirectExchangeRateRequestV1{
-		Code:      "btc-usd",
-		Aggregate: "1s",
-	}
-
-	sub, err := cli.Subscribe(ctx, &request)
-	if err != nil {
-		log.Fatalf("could not subscribe: %v", err)
-	}
-
-	for {
-		elt, err := sub.Recv()
-		if err == io.EOF {
-			return nil
-		}
-
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("[DIRECT EXCHANGE RATE] %+v\n", elt)
-	}
-}
-
-func spotExchangeRateRequest(
-	ctx context.Context,
-	conn *grpc.ClientConn,
-) error {
-	cli := pb.NewStreamAggregatesSpotExchangeRateServiceV1Client(conn)
-	request := aggregates_spot_exchange_rate_v1.StreamAggregatesSpotExchangeRateRequestV1{
-		Code:      "btc-usd",
-		Aggregate: "1s",
-	}
-
-	sub, err := cli.Subscribe(ctx, &request)
-	if err != nil {
-		log.Fatalf("could not subscribe: %v", err)
-	}
-
-	for {
-		elt, err := sub.Recv()
-		if err == io.EOF {
-			return nil
-		}
-
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("[SPOT EXCHANGE RATE] %+v\n", elt)
-	}
-}
-
-func marketUpdateRequest(
-	ctx context.Context,
-	conn *grpc.ClientConn,
-) error {
-	cli := pb.NewStreamMarketUpdateServiceV1Client(conn)
-	// Globbing patterns are also supported on all fields. See http://sdk.kaiko.com/#instrument-selection for all supported patterns
-	request := market_update_v1.StreamMarketUpdateRequestV1{
-		InstrumentCriteria: &core.InstrumentCriteria{
-			Exchange:        "cbse",
-			InstrumentClass: "spot",
-			Code:            "btc-usd",
-		},
-		Commodities: []market_update_v1.StreamMarketUpdateCommodity{market_update_v1.StreamMarketUpdateCommodity_SMUC_TRADE},
-	}
-
-	sub, err := cli.Subscribe(ctx, &request)
-	if err != nil {
-		log.Fatalf("could not subscribe: %v", err)
-	}
-
-	for {
-		elt, err := sub.Recv()
-		if err == io.EOF {
-			return nil
-		}
-
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("[MARKET UPDATE] %+v\n", elt)
-	}
-}
-
 func tradesRequest(
 	ctx context.Context,
 	conn *grpc.ClientConn,
-) error {
+) (int, error) {
 	cli := pb.NewStreamTradesServiceV1Client(conn)
 	// Globbing patterns are also supported on all fields. See http://sdk.kaiko.com/#instrument-selection for all supported patterns
 	request := trades_v1.StreamTradesRequestV1{
 		InstrumentCriteria: &core.InstrumentCriteria{
-			Exchange:        "cbse",
+			Exchange:        "*",
 			InstrumentClass: "spot",
-			Code:            "btc-usd",
+			Code:            "*",
 		},
 	}
 
@@ -293,74 +86,18 @@ func tradesRequest(
 		log.Fatalf("could not subscribe: %v", err)
 	}
 
+	i := 0
+
 	for {
-		elt, err := sub.Recv()
+		_, err := sub.Recv()
 		if err == io.EOF {
-			return nil
+			return i, nil
 		}
 
 		if err != nil {
-			return err
+			return i, err
 		}
 
-		fmt.Printf("[TRADE] %+v\n", elt)
-	}
-}
-
-func indexRequest(
-	ctx context.Context,
-	conn *grpc.ClientConn,
-) error {
-	cli := pb.NewStreamIndexServiceV1Client(conn)
-	request := index_v1.StreamIndexServiceRequestV1{
-		IndexCode: "indexCode", // fill it with actual value
-	}
-
-	sub, err := cli.Subscribe(ctx, &request)
-	if err != nil {
-		log.Fatalf("could not subscribe: %v", err)
-	}
-
-	for {
-		elt, err := sub.Recv()
-		if err == io.EOF {
-			return nil
-		}
-
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("[INDEX] %+v\n", elt)
-	}
-}
-
-func aggregatedQuoteRequest(
-	ctx context.Context,
-	conn *grpc.ClientConn,
-) error {
-	cli := pb.NewStreamAggregatedPriceServiceV1Client(conn)
-	// Globbing patterns are also supported on all fields. See http://sdk.kaiko.com/#instrument-selection for all supported patterns
-	request := aggregated_price_v1.StreamAggregatedPriceRequestV1{
-		InstrumentClass: "spot",
-		Code:            "btc-usd",
-	}
-
-	sub, err := cli.Subscribe(ctx, &request)
-	if err != nil {
-		log.Fatalf("could not subscribe: %v", err)
-	}
-
-	for {
-		elt, err := sub.Recv()
-		if err == io.EOF {
-			return nil
-		}
-
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("[AGGREGATED QUOTE] %+v\n", elt)
+		i++
 	}
 }
