bf89b56f30be0a89e4f4fbe2d99d4237d6756540
killzoner <jean.grimonprez@challenger-deep.com>
2023-04-06 14:52:41 +0200

    update dependencies, bump sdk to 1.7.1

--- examples/scala/project/Dependencies.scala
+++ examples/scala/project/Dependencies.scala
@@ -5,8 +5,8 @@ object Dependencies {
   lazy val grpcTesting =  "io.grpc" % "grpc-testing" % "1.35.0" % Test
   lazy val scalaMock = "org.scalamock" %% "scalamock" % "4.4.0" % Test
 
-  lazy val grpcio = "com.google.protobuf" % "protobuf-java" % "3.19.3"
-  lazy val netty = "io.grpc" % "grpc-netty" % "1.43.2"
-  lazy val sdk = "com.kaiko" %% "scala-sdk" % "1.6.0"
+  lazy val grpcio = "com.google.protobuf" % "protobuf-java" % "3.22.2"
+  lazy val netty = "io.grpc" % "grpc-netty" % "1.54.0"
+  lazy val sdk = "com.kaiko" %% "scala-sdk" % "1.7.1"
   lazy val retry = "com.evanlennick" % "retry4j" % "0.15.0" // optional dependency only to demonstrate resubscribe
 }
