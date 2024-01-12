9e6782161921a1c08bb35c81195afc30fde9892b
killzoner <jean.grimonprez@challenger-deep.com>
2024-01-12 18:10:38 +0100

    update sdk to 1.16.1

--- rust/src/endpoints.rs
+++ rust/src/endpoints.rs
@@ -8,7 +8,7 @@ use tonic::{metadata::Ascii, metadata::MetadataValue, transport::Channel, Reques
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let channel = new_channel().await?;
+    let channel = example::new_channel().await?;
     let api_key = std::env::var("KAIKO_API_KEY").unwrap_or("1234".into());
     let token: MetadataValue<_> = format!("Bearer {}", api_key).parse()?;
 
@@ -77,18 +77,4 @@ async fn client_ohlcv(
     }
 
     Ok(())
-}
-
-async fn new_channel() -> Result<Channel, Box<dyn std::error::Error>> {
-    let connector = hyper_rustls::HttpsConnectorBuilder::new()
-        .with_native_roots()
-        .https_only()
-        .enable_http2()
-        .build();
-
-    let channel = Channel::from_static("https://gateway-v0-grpc.kaiko.ovh:443")
-        .connect_with_connector(connector)
-        .await?;
-
-    Ok(channel)
 }
--- rust/src/resubscribe.rs
+++ rust/src/resubscribe.rs
@@ -9,7 +9,7 @@ use tonic::{metadata::MetadataValue, transport::Channel, Request, Response, Stat
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let channel = new_channel().await?;
+    let channel = example::new_channel().await?;
     let api_key = std::env::var("KAIKO_API_KEY").unwrap_or("1234".into());
     let token: MetadataValue<_> = format!("Bearer {}", api_key).parse()?;
 
@@ -32,20 +32,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .into_inner()
             .take(10);
     }
-}
-
-async fn new_channel() -> Result<Channel, Box<dyn std::error::Error>> {
-    let connector = hyper_rustls::HttpsConnectorBuilder::new()
-        .with_native_roots()
-        .https_only()
-        .enable_http2()
-        .build();
-
-    let channel = Channel::from_static("https://gateway-v0-grpc.kaiko.ovh:443")
-        .connect_with_connector(connector)
-        .await?;
-
-    Ok(channel)
 }
 
 async fn new_sub(
