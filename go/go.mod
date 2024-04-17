e0629d7537e4dafc22ae82dcf16b6234352234ce
killzoner <jean.grimonprez@challenger-deep.com>
2024-04-17 12:08:44 +0200

    Dependency update

--- go/go.mod
+++ go/go.mod
@@ -3,16 +3,16 @@ module example.com/m
 go 1.21
 
 require (
-	github.com/cenkalti/backoff/v4 v4.2.1
-	github.com/kaikodata/kaiko-go-sdk v1.19.1
-	google.golang.org/grpc v1.62.1
+	github.com/cenkalti/backoff/v4 v4.3.0
+	github.com/kaikodata/kaiko-go-sdk v1.20.0
+	google.golang.org/grpc v1.63.2
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240205150955-31a09d347014 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 )
--- scala/project/Dependencies.scala
+++ scala/project/Dependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.3" % Test
-  lazy val grpcTesting =  "io.grpc" % "grpc-testing" % "1.59.0" % Test
+  lazy val grpcTesting =  "io.grpc" % "grpc-testing" % "1.63.0" % Test
   lazy val scalaMock = "org.scalamock" %% "scalamock" % "4.4.0" % Test
 
   lazy val grpcio = "com.google.protobuf" % "protobuf-java" % "3.21.12"
-  lazy val netty = "io.grpc" % "grpc-netty" % "1.59.0"
-  lazy val sdk = "com.kaiko" %% "scala-sdk" % "1.19.1"
+  lazy val netty = "io.grpc" % "grpc-netty" % "1.63.0"
+  lazy val sdk = "com.kaiko" %% "scala-sdk" % "1.20.0"
   lazy val retry = "com.evanlennick" % "retry4j" % "0.15.0" // optional dependency only to demonstrate resubscribe
 }
