d1e8fb3a2484de8bce820841b6e340d6ebd49c5f
killzoner <jean.grimonprez@challenger-deep.com>
2024-08-19 12:29:29 +0200

    bump sdk to 1.22.0

--- cpp/build_docker.sh
+++ cpp/build_docker.sh
@@ -3,6 +3,6 @@
 set -e
 # set -x
 
-docker-compose run --rm --workdir=/build appbuild
-DOCKER_BUILDKIT=1 docker-compose build apprun --no-cache # for debugging build in compose V2: --progress=plain
-docker-compose run --rm -e"KAIKO_API_KEY=${KAIKO_API_KEY}" apprun
+docker compose run --rm --workdir=/build appbuild
+DOCKER_BUILDKIT=1 docker compose build apprun --no-cache # for debugging build in compose V2: --progress=plain
+docker compose run --rm -e"KAIKO_API_KEY=${KAIKO_API_KEY}" apprun
